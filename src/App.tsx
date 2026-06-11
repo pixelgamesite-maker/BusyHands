b5da0c8ec6a146d5d633cafa207668ad45e3e654
earnity-omchain <earnitytempo@gmail.com>
2026-06-11 14:26:58 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { Router as WouterRouter, Route, Switch } from "wouter";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import Home from "@/pages/home";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <TooltipProvider>
+      <WouterRouter>
+        <Router />
+      </WouterRouter>
+      <Toaster />
+    </TooltipProvider>
+  );
+}
+
+export default App;
