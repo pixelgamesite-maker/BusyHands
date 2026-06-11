a6fed7e2a3aed66c007281dc29f564e39e3b5cb9
earnity-omchain <earnitytempo@gmail.com>
2026-06-11 16:18:28 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import { Router as WouterRouter, Route, Switch } from "wouter";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/home";
+import Register from "@/pages/register";
+import Media from "@/pages/media";
+import Checker from "@/pages/checker";
 import NotFound from "@/pages/not-found";
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
+      <Route path="/register" component={Register} />
+      <Route path="/media" component={Media} />
+      <Route path="/checker" component={Checker} />
       <Route component={NotFound} />
     </Switch>
   );
